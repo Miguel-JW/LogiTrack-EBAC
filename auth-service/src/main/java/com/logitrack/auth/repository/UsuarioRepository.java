c75cdbfa31cac0e78c18d914269251e4de99c893
Miguel-JW <conteudojurassicojw@gmail.com>
2026-06-03 09:36:47 -0300

    Create UsuarioRepository.java

---
+++ auth-service/src/main/java/com/logitrack/auth/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.logitrack.auth.repository;
+
+import com.logitrack.auth.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
